1b3189aa04741a53052c152d8a3814b26eb484b1
Nicholas Odenyo <nicksenelwa@gmail.com>
2023-02-20 05:09:56 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,7 +2,8 @@
 
 
 /**
-
+ 
+*  
 * main - prints "with proper grammar, but the outcome is
 * a piece of art," followed by a new line.
 
@@ -16,9 +17,9 @@ int main(void)
 
 {
 
-      printf("with proper grammar, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 
-      return (0);
+return (0);
 
 }
 
