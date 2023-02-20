c6a3502cfba81fbd83c9d6bad26451c4305b8278
Nicholas Odenyo <nicksenelwa@gmail.com>
2023-02-20 02:35:42 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+/**
+ * main - prints "programming is like building a multilingual puzzle, followed by a newline"
+ * return 0
+ */
+int main(void)
+{
+	puts("\"programming is like building a mutilingual puzzle");
+	return(0)
+}
